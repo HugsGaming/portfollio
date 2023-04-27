3bc7673a6d06c8dca85b93529eb7d8730cea51e3
HugsGaming <jovenreyes020803@gmail.com>
2023-04-27 10:23:12 +0800

    Fix Padding

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ProjectCard({image, title, description, url}) {
   return (
-    <a href={url} className='flex flex-col mx-auto items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-xl hover:bg-gray-100 mt-6'>
+    <a href={url} className='flex flex-col mx-auto items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-xl hover:bg-gray-100 mb-5 p-[30px]'>
       <img className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg' src={image} alt={`${title} image`}/>
       <div className='flex flex-col justify-between p-4 leading-normal'>
         <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>{title}</h5>
